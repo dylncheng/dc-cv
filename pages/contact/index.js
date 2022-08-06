aa28faccab0aa4d1c30b3c75fe28baf30432fd63
Dylan Cheng <dylan.cheng@mail.utoronto.ca>
2022-08-06 18:36:13 -0400

    finished responsive typography for contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,6 @@
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
+import { GitHub, LinkedIn, MailOutline }from '@mui/icons-material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import styles from '../../styles/Contact.module.css'
 import HomeButton from "../../components/HomeButton";
@@ -46,9 +47,9 @@ export default function Contact() {
               <Box  className={styles['contact']} width="100%" height="100%" minHeight="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
                   <Typography variant="h1" color="primary">Links</Typography>
                   <ul>
-                      <li className="email"><Typography variant='h6' color='secondary'><a href={"mailto:chengdylan02@gmail.com"} target="_blank" rel="noreferrer">email</a></Typography></li>
-                      <li className="linkedin"><Typography variant='h6' color='secondary'><a href={"https://linkedin.com/in/dylnchng"} target="_blank" rel="noreferrer">linkedin</a></Typography></li>
-                      <li className="github"><Typography variant='h6' color='secondary'><a href={"https://github.com/dylncheng"} target="_blank" rel="noreferrer">github</a></Typography></li>
+                      <li className="email"><Typography variant='h6' color='secondary'><a href={"mailto:chengdylan02@gmail.com"} target="_blank" rel="noreferrer"><MailOutline fontSize="large"></MailOutline></a></Typography></li>
+                      <li className="linkedin"><Typography variant='h6' color='secondary'><a href={"https://linkedin.com/in/dylnchng"} target="_blank" rel="noreferrer"><LinkedIn fontSize="large"></LinkedIn></a></Typography></li>
+                      <li className="github"><Typography variant='h6' color='secondary'><a href={"https://github.com/dylncheng"} target="_blank" rel="noreferrer"><GitHub fontSize="large"></GitHub></a></Typography></li>
                   </ul>
               </Box>
             </div>
